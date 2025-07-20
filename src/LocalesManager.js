ad553f4578c843e7c3d9a7e5905e2f3275c8e180
Sékou SACKO <sekou.sacko@yahoo.fr>
2025-07-20 14:36:45 +0000

    Redirection des dépendances vers le repo github sackofils

--- src/LocalesManager.js
+++ src/LocalesManager.js
@@ -3,7 +3,7 @@ import { fileNamesByLang } from "./locales";
 class LocalesManager {
   getLocale(lang) {
     // messages in requested language are injected as the default 'en' locale
-    return "en";
+    return "fr";
   }
 
   getFileNameByLang(lang) {
